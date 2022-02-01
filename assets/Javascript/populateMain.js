6a8d701e555b9398c6c116a68ff061e44fed5aa6
Anthony <delantho327@gmail.com>
2022-02-01 04:46:13 -0600

    fix dummy json data

--- assets/Javascript/populateMain.js
+++ assets/Javascript/populateMain.js
@@ -1,13 +1,17 @@
-const data = JSON.parse("assets\data\data.json")
-
-for (i = 0; i <= data.length; i++) {
-  const card =
-    `<div class="product" style="width: 18rem;">
-        <img src="${data.main[i].imageSrc}" class="card-img-top" alt="productIMgNotAvailable">
+fetch("data.json")
+.then(res => res.json())
+.then(data => populate(data))
+let populate = (data) => {
+  for (i = 0; i <= data.length; i++) {
+    console.log(data[i])
+    const card =
+      `<div class="product" style="width: 18rem;">
+        <img src="${data[i].imageSrc}" class="card-img-top" alt="productIMgNotAvailable">
         <div class="card-body">
-          <h5 class="card-title">${data.main[i].prodName}</h5>
-          <p class="card-text">${data.main[i].desc}</p>
+          <h5 class="card-title">${data[i].prodName}</h5>
+          <p class="card-text">${data[i].desc}</p>
           <a href="#" class="btn btn-primary">Add to cart</a>
         </div>
       </div>`
-};
+  };
+}
