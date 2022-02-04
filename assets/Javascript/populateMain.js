2843d6f6cbe8334d4869d947a80f1ce9529bdc63
Anthony <delantho327@gmail.com>
2022-02-04 11:38:16 -0600

    fetch dummy items and responsive styling

--- assets/Javascript/populateMain.js
+++ assets/Javascript/populateMain.js
@@ -1,17 +1,24 @@
+let container = document.getElementById("main");
 fetch("data.json")
 .then(res => res.json())
 .then(data => populate(data))
 let populate = (data) => {
-  for (i = 0; i <= data.length; i++) {
-    console.log(data[i])
-    const card =
+  for (i = 0; i <= data.main.length; i++) {
+    console.log(data);
+    let card =
       `<div class="product" style="width: 18rem;">
-        <img src="${data[i].imageSrc}" class="card-img-top" alt="productIMgNotAvailable">
+        <img src= ${data.main[i].imgSrc} class="card-img-top prodImg" alt="productIMgNotAvailable">
         <div class="card-body">
-          <h5 class="card-title">${data[i].prodName}</h5>
-          <p class="card-text">${data[i].desc}</p>
+          <h5 class="card-title">${data.main[i].prodName}</h5>
+          <p class="card-text">${data.main[i].desc}</p>
+          <p class="card-text">${data.main[i].price}</p>
           <a href="#" class="btn btn-primary">Add to cart</a>
         </div>
-      </div>`
+      </div>`;
+      const element = document.createElement('div');
+      element.innerHTML = card;
+      element.className = "col col-lg-2 col-md-4 col-sm-6";
+      console.log(card);
+    container.appendChild(element);
   };
 }
