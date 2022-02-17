c8e1fcc46f29b7eafaf3d0802c30cd8ae3cedffe
Anthony <delantho327@gmail.com>
2022-02-17 17:15:26 -0600

    style cart and catalog

--- assets/Javascript/populateCart.js
+++ assets/Javascript/populateCart.js
@@ -1,4 +1,5 @@
 let cartContainer = document.getElementById("cart");
+let catalogCont = document.getElementById("catalog")
 let dataObj;
 fetch("data.json")
 .then(res => res.json())
@@ -9,13 +10,16 @@ let populateCart = (id) => {
   `<div class="card">
     <h5 class="card-header">${dataObj.main[id].prodName}</h5>
     <div class="card-body">
-      <h5 class="card-title">${dataObj.main[id].desc}</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <p class="card-text">${dataObj.main[id].desc}</p>
+      <div class="row">
+      <div class="col-lg-6"><h5 class="card-title">$${dataObj.main[id].price}</h5></div>
+      <div class="col-lg-6"><a href="#" class="btn btn-primary" style="background-color:transparent; border-color:transparent; color: blue;">Remove</a></div>
+      </div>
     </div>
   </div>`
   let createDiv = document.createElement("div");
   createDiv.innerHTML = cartItem;
   cartContainer.appendChild(createDiv);
-  cartContainer.className = "cart col-lg-3 col-md col-sm"
+  cartContainer.className = "cart col-lg-3 col-md col-sm";
+  catalogCont.className = "catalog col-lg-9 row";
 }
--- assets/Javascript/populateMain.js
+++ assets/Javascript/populateMain.js
@@ -1,22 +1,22 @@
-let container = document.getElementById("main");
+let container = document.getElementById("catalog");
 fetch("data.json")
 .then(res => res.json())
 .then(data => populate(data))
 let populate = (data) => {
   for (i = 0; i <= data.main.length; i++) {
     let card =
-      `<div class="product" style="width: 18rem;">
-        <img src= ${data.main[i].imgSrc} class="card-img-top prodImg" alt="productIMgNotAvailable">
+      `<img src= ${data.main[i].imgSrc} class="card-img-top prodImg" alt="productIMgNotAvailable">
         <div class="card-body">
           <h5 class="card-title">${data.main[i].prodName}</h5>
           <p class="card-text">${data.main[i].desc}</p>
           <p class="card-text">${data.main[i].price}</p>
           <a onClick="populateCart(${data.main[i].id});" class="btn btn-primary">Add to cart</a>
         </div>
-      </div>`;
+        `;
       const element = document.createElement('div');
       element.innerHTML = card;
-      element.className = "col col-lg-2 col-md-4 col-sm-6";
+      element.className = "product col-lg-1;";
+      element.style ="width: 18rem";
     container.appendChild(element);
   };
 }
